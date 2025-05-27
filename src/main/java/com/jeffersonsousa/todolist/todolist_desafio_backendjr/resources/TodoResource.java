5e214a4eb5e884f9208aaee727b5a5662bfc61ae
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-05-27 16:53:35 -0300

    created TodoResource and todo findAll

---
+++ src/main/java/com/jeffersonsousa/todolist/todolist_desafio_backendjr/resources/TodoResource.java
@@ -0,0 +1,36 @@
+package com.jeffersonsousa.todolist.todolist_desafio_backendjr.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeffersonsousa.todolist.todolist_desafio_backendjr.entities.Todo;
+import com.jeffersonsousa.todolist.todolist_desafio_backendjr.services.TodoService;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoResource {
+	
+	@Autowired
+	TodoService todoService;
+	
+	@GetMapping
+    public ResponseEntity<List<Todo>> findAll(){
+    	List <Todo> list = todoService.findAll();
+    	
+    	return ResponseEntity.ok().body(list);
+    }
+
+	//findbyid
+	
+	//insert
+	
+	//delete
+	
+	//update
+
+}
