f93e8234324d24337f96e70fa6c650df63d7c173
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-05-27 17:17:27 -0300

    todo Insert

--- src/main/java/com/jeffersonsousa/todolist/todolist_desafio_backendjr/resources/TodoResource.java
+++ src/main/java/com/jeffersonsousa/todolist/todolist_desafio_backendjr/resources/TodoResource.java
@@ -1,13 +1,17 @@
 package com.jeffersonsousa.todolist.todolist_desafio_backendjr.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jeffersonsousa.todolist.todolist_desafio_backendjr.entities.Todo;
 import com.jeffersonsousa.todolist.todolist_desafio_backendjr.services.TodoService;
@@ -32,7 +36,12 @@ public class TodoResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	//insert
+	@PostMapping
+	public ResponseEntity<Todo> insert(@RequestBody Todo todo){
+		todo = todoService.insert(todo);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(todo.getId()).toUri();
+		return ResponseEntity.created(uri).body(todo);
+	}
 	
 	//delete
 	
